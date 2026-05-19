43d93263d202255b794bb1b558ec17a1157a8cbf
muhammadt90900-ux <muhammadt90900@gmail.com>
2026-05-19 08:12:13 +0300

    Add CarMake and CarModel interfaces

---
+++ packages/types/src/vehicle.ts
@@ -0,0 +1,15 @@
+// packages/types/src/vehicle.ts
+export interface CarMake {
+  id: string;
+  name: MultiLangContent;
+  logoUrl: string;
+  country: string;
+}
+
+export interface CarModel {
+  id: string;
+  makeId: string;
+  name: string;
+  years: number[];
+  bodyType: string;
+}
