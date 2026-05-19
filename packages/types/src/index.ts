2d69a55783a1da1324bab7aa13c420000c4d78f2
muhammadt90900-ux <muhammadt90900@gmail.com>
2026-05-19 08:05:22 +0300

    Create index.ts to export multiple modules

---
+++ packages/types/src/index.ts
@@ -0,0 +1,7 @@
+// packages/types/src/index.ts
+export * from './listing';
+export * from './user';
+export * from './chat';
+export * from './location';
+export * from './vehicle';
+export * from './subscription';
