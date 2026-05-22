41d9607c43f357ecde9904887063fe4e9215d041
muhammadt90900-ux <muhammadt90900@gmail.com>
2026-05-22 09:17:52 +0300

    Rename SubscriptionPage to ReviewsPage

--- apps/web/src/app/[locale]/dashboard/reviews/page.tsx
+++ apps/web/src/app/[locale]/dashboard/reviews/page.tsx
@@ -1,1 +1,1 @@
-export default function SubscriptionPage() { return <div className='p-8'><h1 className='text-2xl font-bold'>Subscription</h1></div> }
+export default function ReviewsPage() { return <div className='p-8'><h1 className='text-2xl font-bold'>Reviews</h1></div> }
