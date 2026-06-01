15bd8285f0c4a55be7988bd60f4c4ea90c9447a5
muhammadt90900-ux <muhammadt90900@gmail.com>
2026-06-01 14:56:46 +0300

    Add files via upload

--- apps/api/src/modules/dealers/dealers.module.ts
+++ apps/api/src/modules/dealers/dealers.module.ts
@@ -4,11 +4,12 @@ import { Module } from '@nestjs/common';
 import { DealersController } from './dealers.controller';
 import { DealersService } from './dealers.service';
 import { PrismaModule } from '@/common/prisma/prisma.module';
+import { EmailVerifiedGuard } from '../../common/guards/email-verified.guard';
 
 @Module({
   imports: [PrismaModule],
   controllers: [DealersController],
-  providers: [DealersService],
+  providers: [DealersService, EmailVerifiedGuard],
   exports: [DealersService],
 })
 export class DealersModule {}
