d53e685eab9c789e8d6aaef5377515f8c6ca7f45
muhammadt90900-ux <muhammadt90900@gmail.com>
2026-06-16 07:02:38 +0300

    Update admin.module.ts

--- apps/api/src/modules/admin/admin.module.ts
+++ apps/api/src/modules/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { PrismaModule } from '../../common/prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
